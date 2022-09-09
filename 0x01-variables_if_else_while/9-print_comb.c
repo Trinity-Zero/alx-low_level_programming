a77eec5fecb8a633ba51960266d5757fc8e3e4d9
Ayodeji <jinayodeji@gmail.com>
2022-09-09 15:52:11 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0
+*/
+int main(void)
+
+{
+	int numbers;
+	int numbers;
+
+	for (numbers = 0; numbers < 10; numbers++)
+	{
+		number = ((numbers % 10) + '0');
+		putchar(number);
+
+		if (number != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
