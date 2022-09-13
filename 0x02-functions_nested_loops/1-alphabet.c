2e650203320bd5c53fd18a47004915d0257829cb
Ayodeji <jinayodeji@gmail.com>
2022-09-13 12:47:50 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_alphabet - Prints lower case letters from a to z
+*
+* Return: void
+*/
+void print_alphabet(void)
+{
+	char letters;
+
+	for (letters = 'a'; letters <= 'Z'; letters++)
+	{
+		_putchar(letters);
+	}
+	_putchar('\n');
+}
