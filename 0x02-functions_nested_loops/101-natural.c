a7fc65831c563ffb6cce5c61e8b00b5bafcca379
Ayodeji <jinayodeji@gmail.com>
2022-09-13 16:26:45 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,12 +12,12 @@ int main(void)
 	int check_5;
 	int total = 0;
 
-	for(i = 0; i <= 1024; i++)
+	for (i = 0; i <= 1024; i++)
 	{
 		check_3 = (i % 3);
 		check_5 = (i % 5);
 
-		if (check_3 ==0 || check_5 == 0)
+		if (check_3 == 0 || check_5 == 0)
 			total += i;
 	}
 
