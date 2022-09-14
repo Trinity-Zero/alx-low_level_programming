18d23ef4dd28f79205a5138add20369632e143b4
Ayodeji <jinayodeji@gmail.com>
2022-09-14 14:34:29 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,20 +8,11 @@
 int main(void)
 {
        int i;
-       long c;
-
-       printf("%ld, ", a);
-       printf("%ld, ", b);
 
        for (i = 3; i <= 98; i++)
        {
-       	       c = a + b;
-               a = b;
-               b = c;
 
-               printf("%ld", c);
-
-               if (i < 98)
+               if (i < 98)
                        printf(",");
                else
                        printf("\n");
