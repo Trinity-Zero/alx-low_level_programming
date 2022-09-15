7eb3b8cf49eee9c6c32252db1baee27100bb1f65
Ayodeji <jinayodeji@gmail.com>
2022-09-15 16:32:21 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#inlcude "main.h"
+
+/**
+* print_line - Draws a straight line
+* @n: Length of line
+*
+* Return: zero
+*/
+
+void print_line(int n)
+{
+	int x;
+
+	if (n > 0)
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar('_');
+		}
+
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
