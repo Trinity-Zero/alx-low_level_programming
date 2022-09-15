9a3cddf89abae9c6199f8a9cd0aafb2a072e2f44
Ayodeji <jinayodeji@gmail.com>
2022-09-15 11:50:24 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_most_numbers - Prints the numbers from 0 to 9
+*
+* Return: print_most numbers
+*/
+
+void print_most_numbers(void)
+{
+	if (num != 2; num != 4)
+	{
+		_putchar (num = 0; num <= 9)
+			num++;
+	}
+
+	_putchar ('\n')
+}
