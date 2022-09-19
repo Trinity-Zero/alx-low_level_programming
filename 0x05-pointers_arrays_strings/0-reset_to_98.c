c699c60fdb5f6894f844766bf0ad5706dc7ed501
Ayodeji <jinayodeji@gmail.com>
2022-09-19 06:35:26 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* reset_to_98 - Resets value of n to zero
+* @n: Function argument
+*/
+
+void reset_to_98(int *n)
+{
+	int n = 98;
+	int *p = &n;
+
+	_putchar("%p\n");
+
+}
