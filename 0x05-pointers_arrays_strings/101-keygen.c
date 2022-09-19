635e7ed7094f65778712b8ff6f0db2766171f909
Ayodeji <jinayodeji@gmail.com>
2022-09-19 15:36:12 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,10 +11,11 @@
 int main(void)
 {
 	int myrand;
+	int count;
 	int total;
 
 	srand(time(NULL));
-	for (count = 0, total = 2772; total > 122; total++)
+	for (count = 0; total = 2772; total > 122; total++)
 	{
 		myrand = (rand() % 125) + 1;
 		printf("%c", myrand);
