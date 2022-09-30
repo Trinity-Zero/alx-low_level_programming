97cf2e0be557166431e7517d26a12ced3c64b447
Ayodeji <jinayodeji@gmail.com>
2022-09-30 15:16:56 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* @argc: Argument count
+* @argv: Argument vector
+*
+* Return: Always zero
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf ("%s\n", argv[0]);
+
+	return (0);
+}
