8f885f253f6b99e18c460600bfd0961161a84a2c
Ayodeji <jinayodeji@gmail.com>
2022-10-01 03:48:06 -0700

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+* coins_check for coins
+* @cent: input
+*
+* Return: coins count
+*/
+
+int coins_check(int cent)
+{
+	int coins = 0, a = 25, b = 10, c = 5, d = 2, e = 1;
+	while (cents > 0)
+	{
+		while (cent >= a)
+		{
+			cents -= a;
+			coins++;
+		}
+
+		while (cents >= b)
+		{
+			cents -= b;
+			coins++;
+		}
+
+		while (cents >= c)
+		{
+			cents -= c;
+			coins++;
+		}
+
+		while (cents >= d)
+		{
+			cents -= d;
+			coins++;
+		}
+
+		while (cents >= e)
+		{
+			cents -= e;
+			coins++;
+		}
+	}
+
+	return (coins);
+}
+
+/**
+* main - Entry point
+* @argc: Argument count
+* @argv: Argument vector
+*
+* Return: Always zero
+*/
+
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else if (atoi(argv[1] < 0))
+	{
+		printf("0\n");
+	}
+	else
+	{
+		cent = atoi(argv[1]);
+		coins = coins_check(cents);
+		printf("%d\n", coins);
+	}
+
+	return (0);
+}
