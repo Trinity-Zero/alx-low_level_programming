e7e0bd8c82f4d3d829136c682e3df5c67b7c5a61
Ayodeji <jinayodeji@gmail.com>
2022-10-01 03:09:53 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+* check_number - Checks if string is a number
+* @s: String
+*
+* Return: 0 if true, 1 otherwise
+*/
+
+int check_number(char *s)
+{
+	int i, check = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (!isdigit(s[1]))
+		{
+			check = 1;
+			break;
+		}
+		else
+			check = 0;
+	}
+
+	return (check);
+}
+
+/**
+* main - Entry point
+* @argc: Argument count
+* @argv: Argument vector
+*
+* Return: Always zero
+*/
+
+int main(int argc, char *argv[])
+{
+	int check = 0, sum = 0;
+	int i, j;
+
+	for (i = 1; i < argc; i++)
+	{
+		check = check_number(argv[i]);
+
+		if (check == 1)
+			break;
+	}
+
+	if (check == 1)
+		printf("Error\n");
+	else
+	{
+		for (j = 1; j < argc; j++)
+			sum += atoi(argv[j]);
+		printf("%d\n", sum);
+	}
+
+	return (0);
+}
