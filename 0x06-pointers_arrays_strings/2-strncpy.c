0736b30bceb1fd04c9762fab144d0abf4e13519d
Ayodeji <jinayodeji@gmail.com>
2022-10-02 03:33:34 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,26 +2,27 @@
 
 /**
 * _strncpy - copies a string
-* @src: second string to copy from
-* @dest: string to be overwritten
-* @n: number of values to copy
-* Description: copies string
-* Return: pointer to dest
+* @src: Source
+* @dest: Destination
+* @n: Limit
+*
+* Return: Destination
 */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int n;
+	int a = 0, b = 0;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
+	while (src[a] != '\0')
+		a++;
 
-	while (i < n)
+	while (b < n)
 	{
-		dest[i] = '\0';
-		i++;
+		if (b < a)
+			dest[b] = src[b];
+		else
+			dest[b] = '\0';
+		b++;
 	}
 
 	return (dest);
