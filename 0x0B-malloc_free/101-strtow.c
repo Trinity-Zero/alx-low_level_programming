6d3efce84228b14529d71b4a9e1e930ca7f5c665
Ayodeji <jinayodeji@gmail.com>
2022-10-04 10:29:09 -0700

    101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* **strtow - function that splits a string into words
+* @str: pointer
+* Return: Char
+*/
+
+char **strtow(char *str)
+{
+	char **d;
+	int count = 0;
+
+	if (str == NULL || *str == ' ')
+		return (NULL);
+
+	while (*str)
+		count++;
+
+	if (count < 1)
+		return (NULL);
+
+	d = malloc(sizeof(char) * 4193);
+	return (d);
+}
