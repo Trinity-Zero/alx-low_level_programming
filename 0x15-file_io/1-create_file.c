8e4855105e491a5b2b9f31c4f68129cf2f464b13
Ayodeji <jinayodeji@gmail.com>
2022-11-01 11:47:25 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fdir = open(filename, O_CREATE | O_WRONLY | O_TRUNC, 0600);
+	fdir = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fdir == -1)
 		return (-1);
