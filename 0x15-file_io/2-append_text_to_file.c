8355d39a60caae8be77716d88c619018192135ef
Ayodeji <jinayodeji@gmail.com>
2022-11-01 12:06:38 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,9 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (n_letters = 0
-; text_content[n_lettets]; n_letters++);
-
+		for (n_letters = 0; text_content[n_lettets]; n_letters++);
 		rwr = write(fdir, text_content, n_letters);
 
 		if (rwr == -1)
