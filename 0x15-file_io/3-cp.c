54cff118e71df17e66e2c27bbb83651645a0301c
Ayodeji <jinayodeji@gmail.com>
2022-11-01 13:12:37 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -54,3 +54,21 @@ int main(int argc, char *argv[])
 			error_file(-1, 0, argv);
 		nwr = write(file_to, buf, n_chars);
 		if (nwr == -1)
+			error_file(0, -1, argv);
+	}
+
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR, FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR, FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
+}
