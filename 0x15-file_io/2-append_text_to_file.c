3f4cf9f310cf24e2c5a4d9f16435f7538b88ff01
Ayodeji <jinayodeji@gmail.com>
2022-11-01 12:03:49 -0700

    2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* append_text_to_file - program appends text at the end of a file
+* @filename: filename
+* @text_content: added content
+*
+* Return: 1 if the file exist, -1 if the file does not exist
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fdir;
+	int n_letters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	fdir = open(filename, O_WRONLY | O_APPEND);
+	if (fdir == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (n_letters = 0; text_content[n_lettets]; n_letters++);
+
+		rwr = write(fdir, text_content, n_letters);
+
+		if (rwr == -1)
+			return (-1);
+	}
+
+	close(fdir);
+
+	return (1);
+}
